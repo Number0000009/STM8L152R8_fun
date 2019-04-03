5cd3ba9d866506378f7c2bf473d84c4d69f276d6
motherf <no.0000000000009@gmail.com>
2019-04-03 15:58:48 +0100

    Add STVD assembler USART1 hello world example

---
+++ STVD_hello_world_asm/main.asm
@@ -0,0 +1,81 @@
+stm8/
+
+	segment 'rom'
+main.l
+
+	mov $50c3, #$20	; CLK_PCKENR1
+	mov $500c, #$08	; PC_DDR USART1.TX is output
+									; PC_CR1 USART1.TX is pseudo open drain
+	mov $5232, #$0d	; BRR1
+
+; without this first char is crap
+	ld a, #$ff
+wait:
+	nop
+	nop
+	nop
+	nop
+	dec a
+	jrne wait
+	
+	mov $5235, #$0c	; CR2 Enable TX and RX
+
+loop:
+
+	ld a, (msg,x)
+	jreq exit
+
+	btjf $5230, #7, *			; USART1.SR[TXE]
+
+	ld $5231, a						; USART1.DR
+
+	incw x
+	jra loop
+
+exit:
+												; USART1.SR[TC]
+	btjf $5230, #6, *			; wait for previous transmission to complete
+
+	halt
+	
+msg
+	dc.b "Anime is bad", $0A, $0D, 0
+
+	interrupt NonHandledInterrupt
+NonHandledInterrupt.l
+	iret
+
+	segment 'vectit'
+	dc.l {$82000000+main}									; reset
+	dc.l {$82000000+NonHandledInterrupt}	; trap
+	dc.l {$82000000+NonHandledInterrupt}	; irq0
+	dc.l {$82000000+NonHandledInterrupt}	; irq1
+	dc.l {$82000000+NonHandledInterrupt}	; irq2
+	dc.l {$82000000+NonHandledInterrupt}	; irq3
+	dc.l {$82000000+NonHandledInterrupt}	; irq4
+	dc.l {$82000000+NonHandledInterrupt}	; irq5
+	dc.l {$82000000+NonHandledInterrupt}	; irq6
+	dc.l {$82000000+NonHandledInterrupt}	; irq7
+	dc.l {$82000000+NonHandledInterrupt}	; irq8
+	dc.l {$82000000+NonHandledInterrupt}	; irq9
+	dc.l {$82000000+NonHandledInterrupt}	; irq10
+	dc.l {$82000000+NonHandledInterrupt}	; irq11
+	dc.l {$82000000+NonHandledInterrupt}	; irq12
+	dc.l {$82000000+NonHandledInterrupt}	; irq13
+	dc.l {$82000000+NonHandledInterrupt}	; irq14
+	dc.l {$82000000+NonHandledInterrupt}	; irq15
+	dc.l {$82000000+NonHandledInterrupt}	; irq16
+	dc.l {$82000000+NonHandledInterrupt}	; irq17
+	dc.l {$82000000+NonHandledInterrupt}	; irq18
+	dc.l {$82000000+NonHandledInterrupt}	; irq19
+	dc.l {$82000000+NonHandledInterrupt}	; irq20
+	dc.l {$82000000+NonHandledInterrupt}	; irq21
+	dc.l {$82000000+NonHandledInterrupt}	; irq22
+	dc.l {$82000000+NonHandledInterrupt}	; irq23
+	dc.l {$82000000+NonHandledInterrupt}	; irq24
+	dc.l {$82000000+NonHandledInterrupt}	; irq25
+	dc.l {$82000000+NonHandledInterrupt}	; irq26
+	dc.l {$82000000+NonHandledInterrupt}	; irq27
+	dc.l {$82000000+NonHandledInterrupt}	; irq28
+	dc.l {$82000000+NonHandledInterrupt}	; irq29
+	end
